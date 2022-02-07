295c9526a3573122f8a379d9779e3be12b1ad0bf
Nil Borodulia <nborodulia@gmail.com>
2022-02-07 17:37:53 +0200

    add voiptime

--- public/index.php
+++ public/index.php
@@ -2,29 +2,7 @@
 //'/clients/exec.do';
 $voip = new Voiptime($login, $passwod);
 
-class Voiptime
-{
-    public const API_URL = "https://smart-sms.voiptime.net/api/v1";
-    
-    private string $login;
-    private string $password;
-    
-    public function __construct(string $login, string $password)
-    {
-        $this->login = $login;
-        $this->password = $password;
-    }
-    
-    public function createClients(bool $recovery, Client ...$clients)
-    {
-        $data['recovery'] = $recovery;
-        
-        foreach ($clients as $client) {
-            $data['clients'][] = $client->toArray();
-        }
-        
-    }
-}
+
 
 class Client
 {
